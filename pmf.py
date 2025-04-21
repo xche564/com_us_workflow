c33b9fa690467ec17cf17754d5670b2e23519ceb
Xu Chen <xche564@login03.chn.perlmutter.nersc.gov>
2025-04-20 18:06:04 -0700

    correct the pmf setting

--- pmf.py
+++ pmf.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 num_windows = 30
-max_dist = 33
-min_dist = 3
+NORMALIZE = True
+min_com = 3.0
+max_com = 33.0
+window_centers = np.linspace(min_com, max_com, num_windows)
 
 with open("metadata.dat", "w") as f:
-    for i in range(0, num_windows):
-        f.write(f"window_{i}/dist.dat {i} 4\n")
+    for i in range(num_windows):
+        f.write(f"window_{i}/dist.dat {window_centers[i]} 2.0\n")
 
-os.system(f"/home/xuchen/rfmm/wham/wham/wham {min_dist-1} {max_dist-3} 100 0.01 298.15 0 metadata.dat out.pmf")
+os.system(f"/pscratch/sd/x/xche564/bdp/wham/wham/wham {min_com} {max_com} 100 0.01 298.15 0 metadata.dat out.pmf")
 
 # Delete the first line of the file
 os.system("tail -n +2 out.pmf > temp.pmf && mv temp.pmf out.pmf")
@@ -28,15 +30,16 @@ with open('pmf.dat') as f:
         cols = line.split()
         x.append(float(cols[0]))
         y.append(float(cols[1]))
+if NORMALIZE:
+    # remove inf and nan
+    mask = np.isfinite(y)
+    x = np.array(x)[mask]
+    y = np.array(y)[mask]
 
-# remove inf and nan
-mask = np.isfinite(y)
-x = np.array(x)[mask]
-y = np.array(y)[mask]
 
-last_34_values = y[-34:]
-average_y = sum(last_34_values) / len(last_34_values)
-y = [i - average_y for i in y]
+    last_34_values = y[-34:]
+    average_y = sum(last_34_values) / len(last_34_values)
+    y = [i - average_y for i in y]
 
 # Create plot
 plt.plot(x, y)
